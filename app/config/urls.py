468ea7ec16f2f8906f3fcdbe72d6d60333a37653
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-09-26 18:00:23 +0300

    Simplified the project URL configuration by importing both include and path directly from django.urls

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls.conf import include
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
