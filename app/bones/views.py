6780d7c8044fa5d9eda5c70a8da34f7c07d671f7
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-09-29 14:58:27 +0300

    Refine bones domain models

--- app/bones/views.py
+++ app/bones/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.db import DatabaseError
 
-from .models import Completedtransects
+from .models import CompletedTransect
 
 
 def hello_world(request):
     """Render a simple page with the CompletedTransects count."""
     try:
-        completed_transects_count = Completedtransects.objects.count()
+        completed_transects_count = CompletedTransect.objects.count()
     except DatabaseError:
         completed_transects_count = None
 
