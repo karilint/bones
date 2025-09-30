32663e09159890da9c3be52ca9b10074a99c505e
Kari Lintulaakso <kari.lintulaakso@gmail.com>
2025-09-30 10:11:43 +0300

    Harden list filter mixin against database outages

--- app/bones/urls.py
+++ app/bones/urls.py
@@ -1,7 +1,72 @@
-from django.urls import path
+from django.urls import include, path
 
-from . import views
+from .views import (
+    CompletedOccurrenceListView,
+    CompletedTransectListView,
+    CompletedWorkflowListView,
+    DashboardView,
+    DataLogFileListView,
+    DataTypeListView,
+    DataTypeOptionListView,
+    ProjectConfigListView,
+    QuestionListView,
+    TemplateTransectListView,
+)
+
+app_name = "bones"
+
+transect_patterns = (
+    [
+        path("", CompletedTransectListView.as_view(), name="list"),
+    ],
+    "transects",
+)
+
+occurrence_patterns = (
+    [
+        path("", CompletedOccurrenceListView.as_view(), name="list"),
+    ],
+    "occurrences",
+)
+
+workflow_patterns = (
+    [
+        path("", CompletedWorkflowListView.as_view(), name="list"),
+    ],
+    "workflows",
+)
+
+template_patterns = (
+    [
+        path("", TemplateTransectListView.as_view(), name="list"),
+        path("questions/", QuestionListView.as_view(), name="questions"),
+    ],
+    "templates",
+)
+
+reference_patterns = (
+    [
+        path("", DataTypeListView.as_view(), name="list"),
+        path("data-types/", DataTypeListView.as_view(), name="data_types"),
+        path("data-type-options/", DataTypeOptionListView.as_view(), name="data_type_options"),
+        path("project-configs/", ProjectConfigListView.as_view(), name="project_config"),
+    ],
+    "reference",
+)
+
+log_patterns = (
+    [
+        path("", DataLogFileListView.as_view(), name="list"),
+    ],
+    "logs",
+)
 
 urlpatterns = [
-    path("", views.hello_world, name="hello_world"),
+    path("", DashboardView.as_view(), name="dashboard"),
+    path("transects/", include(transect_patterns)),
+    path("occurrences/", include(occurrence_patterns)),
+    path("workflows/", include(workflow_patterns)),
+    path("templates/", include(template_patterns)),
+    path("reference/", include(reference_patterns)),
+    path("logs/", include(log_patterns)),
 ]
